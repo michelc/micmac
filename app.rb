dcaeb9a6f13ed27710c4e284c27841d0447b2b9b
michelc <michelc@gmail.com>
2012-06-12 08:33:30 +0200

    Première étape pour utiliser une vraie bdd

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 # encoding: UTF-8
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
@@ -9,7 +10,7 @@ configure do
 end
 
 
-class Carte
+class Carte_QD
   attr_accessor :url, :titre
 
   def initialize(url, titre)
@@ -47,36 +48,36 @@ def quick_and_dirty_db()
   # Initialise la liste des cartes postales
   cartes = []
   # Insére les différentes cartes postales
-  cartes << Carte.new("01-ardeche", "L'Ardèche à Saint-Privat")
-  cartes << Carte.new("02-ardeche", "L'Ardèche à Saint-Privat")
-  cartes << Carte.new("03-vue-generale", "Vue Générale")
-  cartes << Carte.new("04-rue-principale", "Rue Principale")
-  cartes << Carte.new("05-le-barrage", "Le Barrage sur le Luol")
-  cartes << Carte.new("06-vue-generale-aerienne", "Vue Générale Aérienne")
-  cartes << Carte.new("07-pont-luol", "Pont sur le Luol")
-  cartes << Carte.new("08-usine-charnivet", "Usine du Charnivet")
-  cartes << Carte.new("09-rue-principale", "Rue Principale")
-  cartes << Carte.new("10-vue-generale", "Vue Générale")
-  cartes << Carte.new("11-vue-generale-aerienne", "Vue Générale Aérienne")
-  cartes << Carte.new("12-vue-generale-aerienne", "Vue Générale Aérienne")
-  cartes << Carte.new("13-vue-generale", "Vue Générale")
-  cartes << Carte.new("14-vue-generale", "Vue Générale")
-  cartes << Carte.new("15-rue-village", "Rue Village")
-  cartes << Carte.new("16-eglise", "L'Eglise")
-  cartes << Carte.new("17-vue-generale-aerienne", "Vue Générale Aérienne")
-  cartes << Carte.new("18-marche-aux-fruits", "Le Marché aux Fruits")
-  cartes << Carte.new("19-marche-aux-fruits", "Le Marché aux Fruits")
-  cartes << Carte.new("20-marche-aux-fruits", "Le Marché aux Fruits")
-  cartes << Carte.new("21-vergers", "Vergers vers les Cigales")
-  cartes << Carte.new("22-terrain-municipal", "Terrain Municipal")
-  cartes << Carte.new("23-entree-propriete", "Entrée des Cigales")
-  cartes << Carte.new("24-terrain-sport", "Terrain de Sport")
-  cartes << Carte.new("25-terrain-sport", "Terrain de Sport")
-  cartes << Carte.new("26-batiment-cigales", "Bâtiment des Cigales")
-  cartes << Carte.new("27-route-nationale", "Route Nationale")
-  cartes << Carte.new("28-batiment-cigales", "Bâtiment des Cigales")
-  cartes << Carte.new("29-vue-generale-aerienne", "Vue Générale Aérienne")
-  cartes << Carte.new("30-vue-generale-aerienne", "Vue Générale Aérienne")
+  cartes << Carte_QD.new("01-ardeche", "L'Ardèche à Saint-Privat")
+  cartes << Carte_QD.new("02-ardeche", "L'Ardèche à Saint-Privat")
+  cartes << Carte_QD.new("03-vue-generale", "Vue Générale")
+  cartes << Carte_QD.new("04-rue-principale", "Rue Principale")
+  cartes << Carte_QD.new("05-le-barrage", "Le Barrage sur le Luol")
+  cartes << Carte_QD.new("06-vue-generale-aerienne", "Vue Générale Aérienne")
+  cartes << Carte_QD.new("07-pont-luol", "Pont sur le Luol")
+  cartes << Carte_QD.new("08-usine-charnivet", "Usine du Charnivet")
+  cartes << Carte_QD.new("09-rue-principale", "Rue Principale")
+  cartes << Carte_QD.new("10-vue-generale", "Vue Générale")
+  cartes << Carte_QD.new("11-vue-generale-aerienne", "Vue Générale Aérienne")
+  cartes << Carte_QD.new("12-vue-generale-aerienne", "Vue Générale Aérienne")
+  cartes << Carte_QD.new("13-vue-generale", "Vue Générale")
+  cartes << Carte_QD.new("14-vue-generale", "Vue Générale")
+  cartes << Carte_QD.new("15-rue-village", "Rue Village")
+  cartes << Carte_QD.new("16-eglise", "L'Eglise")
+  cartes << Carte_QD.new("17-vue-generale-aerienne", "Vue Générale Aérienne")
+  cartes << Carte_QD.new("18-marche-aux-fruits", "Le Marché aux Fruits")
+  cartes << Carte_QD.new("19-marche-aux-fruits", "Le Marché aux Fruits")
+  cartes << Carte_QD.new("20-marche-aux-fruits", "Le Marché aux Fruits")
+  cartes << Carte_QD.new("21-vergers", "Vergers vers les Cigales")
+  cartes << Carte_QD.new("22-terrain-municipal", "Terrain Municipal")
+  cartes << Carte_QD.new("23-entree-propriete", "Entrée des Cigales")
+  cartes << Carte_QD.new("24-terrain-sport", "Terrain de Sport")
+  cartes << Carte_QD.new("25-terrain-sport", "Terrain de Sport")
+  cartes << Carte_QD.new("26-batiment-cigales", "Bâtiment des Cigales")
+  cartes << Carte_QD.new("27-route-nationale", "Route Nationale")
+  cartes << Carte_QD.new("28-batiment-cigales", "Bâtiment des Cigales")
+  cartes << Carte_QD.new("29-vue-generale-aerienne", "Vue Générale Aérienne")
+  cartes << Carte_QD.new("30-vue-generale-aerienne", "Vue Générale Aérienne")
   # Renvoie la liste des cartes postales
   cartes
 end
